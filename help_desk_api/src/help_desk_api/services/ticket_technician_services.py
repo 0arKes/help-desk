c7f8889a456424aaf80f3fcc73aa44930b169854
0arKes <proarKes@gmail.com>
2026-07-16 15:13:20 -0300

    Historico para technician
    Criando histórico para as rotas de technician

--- help_desk_api/src/help_desk_api/services/ticket_technician_services.py
+++ help_desk_api/src/help_desk_api/services/ticket_technician_services.py
@@ -1,8 +1,10 @@
+from help_desk_api.db.enum.history_actions import HistoryAction
 from help_desk_api.db.enum.ticket_priority import TicketPriority
 from help_desk_api.db.enum.ticket_status import TicketStatus
 from help_desk_api.db.enum.user_role import UserRole
 from help_desk_api.db.models.ticket import Ticket
 from help_desk_api.db.models.user import User
+from help_desk_api.services.history import create_ticket_history
 from help_desk_api.services.ticket_core_services import (
     get_ticket_by_id,
     validate_require_role,
@@ -25,7 +27,7 @@ def queue_without_responsible(user: User, session: Session):
 
     queue = session.scalars(
         select(Ticket)
-        .where(Ticket.responsible_id == None)
+        .where(Ticket.responsible_id.is_(None))
         .where(Ticket.status == TicketStatus.OPEN)
         .options(joinedload(Ticket.creator), joinedload(Ticket.responsible))
         .order_by(priority_order.desc(), Ticket.created_at.asc())
@@ -40,8 +42,17 @@ def assign_responsible_technician(id: int, user: User, session: Session):
     validate_ticket_not_assigned(ticket)
     validate_ticket_not_resolved(ticket)
 
+    history = create_ticket_history(
+        ticket,
+        HistoryAction.ASSIGNED,
+        user,
+        TicketStatus.OPEN.value,
+        TicketStatus.IN_PROGRESS.value,
+    )
     ticket.responsible = user
     ticket.status = TicketStatus.IN_PROGRESS
+
+    session.add(history)
     session.commit()
     session.refresh(ticket)
 
@@ -54,8 +65,18 @@ def remove_responsible_ticket_technician(id: int, user: User, session: Session):
     validate_ticket_owner(ticket, user)
     validate_ticket_not_resolved(ticket)
 
+    history = create_ticket_history(
+        ticket,
+        HistoryAction.UNASSIGNED,
+        user,
+        _old_value=TicketStatus.IN_PROGRESS.value,
+        _new_value=TicketStatus.OPEN.value,
+    )
+
     ticket.responsible = None
     ticket.status = TicketStatus.OPEN
+
+    session.add(history)
     session.commit()
     session.refresh(ticket)
 
@@ -80,7 +101,17 @@ def resolve_ticket_by_id(id: int, user: User, session: Session):
     validate_ticket_owner(ticket, user)
     validate_ticket_not_resolved(ticket)
 
+    history = create_ticket_history(
+        ticket,
+        HistoryAction.RESOLVED,
+        user,
+        _old_value=TicketStatus.IN_PROGRESS.value,
+        _new_value=TicketStatus.RESOLVED.value,
+    )
+
     ticket.status = TicketStatus.RESOLVED
+
+    session.add(history)
     session.commit()
     session.refresh(ticket)
 
